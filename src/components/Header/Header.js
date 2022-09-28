df50c95c65eadd6cdaf228043357109c0cbaa34b
Md-Mubarak-Hossain <hossainmubarak477@gmail.com>
2022-09-29 02:45:23 +0600

    added bootstrap5.2 and header name with folder create

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div className='header'>
+
+            <h3 className='fs-1'>Daily Health Activities</h3>
+        </div>
+    );
+};
+
+export default Header;
